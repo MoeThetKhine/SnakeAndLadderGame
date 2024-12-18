d9db7daabb5a9fe785a64be0534f38b125e24f87
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:41:41 +0630

    Feat : Add StartGameRequestModel

---
+++ SnakeAndLadderGame.Domain/Models/StartGame/StartGameRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models.StartGame
+{
+    public class StartGameRequestModel
+    {
+        public string BoardID { get; set; }
+        public List<string> PlayerIDs { get; set; }
+    }
+}
