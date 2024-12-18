eb8f81ffaf025fddce84f845773e0bd608db9429
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 22:02:03 +0630

    -

--- SnakeAndLadderGame.Domain/Models/Player/PlayerDetailModel.cs
+++ SnakeAndLadderGame.Domain/Models/Player/PlayerDetailModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace SnakeAndLadderGame.Domain.Models.Player;
 
+#region PlayerDetailModel
+
 public class PlayerDetailModel
 {
     public string PlayerID { get; set; }
     public string Color { get; set; }
     public int CurrentPosition { get; set; }
 }
+
+#endregion
