8dfe61a7a10199cce4210c0c4b022a0d5a85f598
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:41:04 +0630

    Format Code

--- SnakeAndLadderGame.Domain/Models/GameWinner/GameWinnerModel.cs
+++ SnakeAndLadderGame.Domain/Models/GameWinner/GameWinnerModel.cs
@@ -1,14 +1,13 @@
-﻿namespace SnakeAndLadderGame.Domain.Models.GameWinner
+﻿namespace SnakeAndLadderGame.Domain.Models.GameWinner;
+
+public class GameWinnerModel
 {
-    public class GameWinnerModel
-    {
-        public string GameWinnerId { get; set; } = null!;
+    public string GameWinnerId { get; set; } = null!;
 
-        public string GameId { get; set; } = null!;
+    public string GameId { get; set; } = null!;
 
-        public string PlayerId { get; set; } = null!;
+    public string PlayerId { get; set; } = null!;
 
-        public int Rank { get; set; }
+    public int Rank { get; set; }
 
-    }
 }
