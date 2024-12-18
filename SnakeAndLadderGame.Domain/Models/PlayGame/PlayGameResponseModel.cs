6482c0d56aa893fb5e2ffe56731ae5d0bb581059
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:44:04 +0630

    Format Code

--- SnakeAndLadderGame.Domain/Models/PlayGame/PlayGameResponseModel.cs
+++ SnakeAndLadderGame.Domain/Models/PlayGame/PlayGameResponseModel.cs
@@ -1,13 +1,12 @@
-﻿namespace SnakeAndLadderGame.Domain.Models.PlayGame
+﻿namespace SnakeAndLadderGame.Domain.Models.PlayGame;
+
+public class PlayGameResponseModel
 {
-    public class PlayGameResponseModel
-    {
-        public string PlayerID { get; set; }
-        public string Color { get; set; }
-        public int DiceRoll { get; set; }
-        public int PreviousPosition { get; set; }
-        public int NewPosition { get; set; }
-        public string Status { get; set; }
-        public string Message { get; set; }
-    }
+    public string PlayerID { get; set; }
+    public string Color { get; set; }
+    public int DiceRoll { get; set; }
+    public int PreviousPosition { get; set; }
+    public int NewPosition { get; set; }
+    public string Status { get; set; }
+    public string Message { get; set; }
 }
