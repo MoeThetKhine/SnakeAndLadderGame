0aa3f8ef58b30dcc0056f64e0e5badc906f5db1e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:57:51 +0630

    Feat : Add DI

--- SnakeAndLadderGame.RestApi/Program.cs
+++ SnakeAndLadderGame.RestApi/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SnakeandLadderGame.Database.Models;
+using SnakeAndLadderGame.Domain.Features.Game;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+builder.Services.AddScoped<GameService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
