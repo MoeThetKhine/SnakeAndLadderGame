450b54d60e5c4ca27839041e560829b6cb651ebc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:42:15 +0630

    -

--- SnakeAndLadderGame.Domain/Features/Game/GameService.cs
+++ SnakeAndLadderGame.Domain/Features/Game/GameService.cs
@@ -1,5 +1,13 @@
-﻿namespace SnakeAndLadderGame.Domain.Features.Game;
+﻿using SnakeandLadderGame.Database.Models;
+
+namespace SnakeAndLadderGame.Domain.Features.Game;
 
 public class GameService
 {
+    private readonly AppDbContext _context;
+
+    public GameService(AppDbContext context)
+    {
+        _context = context;
+    }
 }
