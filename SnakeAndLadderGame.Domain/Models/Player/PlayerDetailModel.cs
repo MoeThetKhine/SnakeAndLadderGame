652fca46d562df2151077ed5897dce8bfb1f8d3f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:38:16 +0630

    Feat : Add PlayerDetailModel

---
+++ SnakeAndLadderGame.Domain/Models/Player/PlayerDetailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SnakeAndLadderGame.Domain.Models.Player
+{
+    public class PlayerDetailModel
+    {
+        public string PlayerID { get; set; }
+        public string Color { get; set; }
+        public int CurrentPosition { get; set; }
+    }
+}
