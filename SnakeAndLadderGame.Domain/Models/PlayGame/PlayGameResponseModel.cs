4298fcd195007ee1dff20b1eb0a57291e0a50f79
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:41:20 +0630

    Feat : Add PlayGameResponseModel

---
+++ SnakeAndLadderGame.Domain/Models/PlayGame/PlayGameResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models.PlayGame
+{
+    public class PlayGameResponseModel
+    {
+        public string PlayerID { get; set; }
+        public string Color { get; set; }
+        public int DiceRoll { get; set; }
+        public int PreviousPosition { get; set; }
+        public int NewPosition { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
