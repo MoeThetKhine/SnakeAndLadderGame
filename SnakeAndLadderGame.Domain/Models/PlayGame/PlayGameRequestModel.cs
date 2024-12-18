bc5a0c71781a34fa0bd42f050432cdc1948f89d3
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:40:54 +0630

    Feat : Add PlayGameRequestModel

---
+++ SnakeAndLadderGame.Domain/Models/PlayGame/PlayGameRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models.PlayGame
+{
+    public class PlayGameRequestModel
+    {
+        public string GameID { get; set; }
+        public string PlayerID { get; set; }
+    }
+}
