42efef6be6567679c8dfd51b3dedb98416a7b786
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:52:38 +0630

    Feat : Add EnumRespType

---
+++ SnakeAndLadderGame.Domain/Models/EnumRespType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models
+{
+    public enum EnumRespType
+    {
+        None,
+        Success,
+        ValidationError,
+        SystemError
+    }
+}
