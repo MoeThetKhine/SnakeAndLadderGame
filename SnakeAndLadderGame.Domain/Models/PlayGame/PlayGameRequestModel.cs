3c5a4da3818a08187d80066d30de03d4175b3f84
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:44:15 +0630

    -

--- SnakeAndLadderGame.Domain/Models/PlayGame/PlayGameRequestModel.cs
+++ SnakeAndLadderGame.Domain/Models/PlayGame/PlayGameRequestModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeAndLadderGame.Domain.Models.PlayGame
+﻿namespace SnakeAndLadderGame.Domain.Models.PlayGame
 {
     public class PlayGameRequestModel
     {
