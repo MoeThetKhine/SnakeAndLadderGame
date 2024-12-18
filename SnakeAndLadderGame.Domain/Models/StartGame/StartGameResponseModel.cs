bd527651b08fe372538cc5b1ccc812b28f73cb9b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:47:27 +0630

    -

--- SnakeAndLadderGame.Domain/Models/StartGame/StartGameResponseModel.cs
+++ SnakeAndLadderGame.Domain/Models/StartGame/StartGameResponseModel.cs
@@ -2,9 +2,13 @@
 
 namespace SnakeAndLadderGame.Domain.Models.StartGame;
 
+#region StartGameResponseModel
+
 public class StartGameResponseModel
 {
     public string GameID { get; set; }
     public string BoardID { get; set; }
     public List<PlayerDetailModel> Players { get; set; }
 }
+
+#endregion
