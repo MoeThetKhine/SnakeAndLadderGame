683c5ad96fa79f9d5a3ae35b3c8fddac00ebd8ed
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 17:59:05 +0630

    -

--- SnakeandLadderGame.Database/Models/TblGameWinner.cs
+++ SnakeandLadderGame.Database/Models/TblGameWinner.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace SnakeandLadderGame.Database.Models;
+﻿namespace SnakeandLadderGame.Database.Models;
 
 public partial class TblGameWinner
 {
