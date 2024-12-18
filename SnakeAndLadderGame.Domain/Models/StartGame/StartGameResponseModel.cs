be2331e9e7de0f35a06144413d18171e3d12d0f2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 21:42:04 +0630

    Feat : Add StartGameResponseModel

---
+++ SnakeAndLadderGame.Domain/Models/StartGame/StartGameResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using SnakeAndLadderGame.Domain.Models.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models.StartGame
+{
+    public class StartGameResponseModel
+    {
+        public string GameID { get; set; }
+        public string BoardID { get; set; }
+        public List<PlayerDetailModel> Players { get; set; }
+    }
+}
