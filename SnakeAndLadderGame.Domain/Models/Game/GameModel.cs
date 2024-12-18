333cebc815a1e16d6a3ecdf0c4f35dd74d6bd707
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:41:48 +0630

    Format Code

--- SnakeAndLadderGame.Domain/Models/Game/GameModel.cs
+++ SnakeAndLadderGame.Domain/Models/Game/GameModel.cs
@@ -1,15 +1,14 @@
-﻿namespace SnakeAndLadderGame.Domain.Models.Game
+﻿namespace SnakeAndLadderGame.Domain.Models.Game;
+
+public class GameModel
 {
-    public class GameModel
-    {
-        public string GameId { get; set; } = null!;
+    public string GameId { get; set; } = null!;
 
-        public string BoardId { get; set; } = null!;
+    public string BoardId { get; set; } = null!;
 
-        public string? GameStatus { get; set; }
+    public string? GameStatus { get; set; }
 
-        public DateTime StartDate { get; set; }
+    public DateTime StartDate { get; set; }
 
-        public DateTime? EndDate { get; set; }
-    }
+    public DateTime? EndDate { get; set; }
 }
