73a6933751d8df45e8a52c3caa8d2c638924b6d7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:37:00 +0630

    Feat : Add GamePlayerModel

---
+++ SnakeAndLadderGame.Domain/Models/GamePlayer/GamePlayerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models.GamePlayer
+{
+    public class GamePlayerModel
+    {
+        public string GamePlayerId { get; set; } = null!;
+
+        public string GameId { get; set; } = null!;
+
+        public string PlayerId { get; set; } = null!;
+
+        public string? Color { get; set; }
+
+        public int CurrentPosition { get; set; }
+
+        public string? PlayerStatus { get; set; }
+    }
+}
