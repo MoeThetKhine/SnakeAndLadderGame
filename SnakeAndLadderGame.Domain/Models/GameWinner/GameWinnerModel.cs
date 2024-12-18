fc1dbb5d5f524baa00bce49efb7020e387025775
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:37:23 +0630

    Feat : GameWinnerModel

---
+++ SnakeAndLadderGame.Domain/Models/GameWinner/GameWinnerModel.cs
@@ -0,0 +1,21 @@
+﻿using SnakeandLadderGame.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame.Domain.Models.GameWinner
+{
+    public class GameWinnerModel
+    {
+        public string GameWinnerId { get; set; } = null!;
+
+        public string GameId { get; set; } = null!;
+
+        public string PlayerId { get; set; } = null!;
+
+        public int Rank { get; set; }
+
+    }
+}
