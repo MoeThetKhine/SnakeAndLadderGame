20428ed774b87b6639aebf1f184cb241da9a1cf0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-18 20:41:16 +0630

    -

--- SnakeAndLadderGame.Domain/Models/GamePlayer/GamePlayerModel.cs
+++ SnakeAndLadderGame.Domain/Models/GamePlayer/GamePlayerModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeAndLadderGame.Domain.Models.GamePlayer
+﻿namespace SnakeAndLadderGame.Domain.Models.GamePlayer
 {
     public class GamePlayerModel
     {
