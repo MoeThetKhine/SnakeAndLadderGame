28beea3fbd5a72e10c17d15bc26311a8fd7cabf0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 10:46:43 +0630

    Feat : CreatePlayer in PlayerController

---
+++ SnakeAndLadderGame.RestApi/Controllers/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SnakeAndLadderGame.Domain.Features.Player;
+using SnakeAndLadderGame.Domain.Models.Player;
+
+namespace SnakeAndLadderGame.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly PlayerService _playerService;
+
+        public PlayerController(PlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreatePlayer([FromBody] PlayerRequestModel request)
+        {
+           
+
+            var result = await _playerService.CreatePlayerAsync(request);
+
+            return Ok(result);
+        }
+    }
+}
