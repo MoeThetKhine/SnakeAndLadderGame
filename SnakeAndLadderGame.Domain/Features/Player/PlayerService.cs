57ca1aac71870c6783c251b81bf206f508625b01
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-20 10:55:01 +0630

    Format Code

--- SnakeAndLadderGame.Domain/Features/Player/PlayerService.cs
+++ SnakeAndLadderGame.Domain/Features/Player/PlayerService.cs
@@ -1,73 +1,72 @@
-﻿namespace SnakeAndLadderGame.Domain.Features.Player
+﻿namespace SnakeAndLadderGame.Domain.Features.Player;
+
+public class PlayerService
 {
-    public class PlayerService
+    private readonly AppDbContext _context;
+
+    public PlayerService(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _context;
+        _context = appDbContext;
+    }
 
-        public PlayerService(AppDbContext appDbContext)
+    public async Task<Result<PlayerRequestModel>> CreatePlayerAsync(PlayerRequestModel request)
+    {
+        try
         {
-            _context = appDbContext;
+            #region Find Last Player
+
+            var lastPlayer = await _context.TblPlayers
+                .OrderByDescending(p => p.PlayerId)
+                .FirstOrDefaultAsync();
+
+            #endregion
+
+            #region Create Player Id
+
+            int nextId = 1;
+            if (lastPlayer is not null && lastPlayer.PlayerId.StartsWith("p"))
+            {
+                int.Parse(lastPlayer.PlayerId.Substring(1));
+                nextId++;
+            }
+
+            string newPlayerId = $"p{nextId}";
+
+            #endregion
+
+            #region Validation for Player Name and Email
+
+            if (string.IsNullOrEmpty(request.PlayerName)) 
+            {
+                return Result<PlayerRequestModel>.ValidationError("Player Name cannot be empty ");
+            }
+
+            if(string.IsNullOrEmpty(request.Email))
+            {
+                return Result<PlayerRequestModel>.ValidationError("Email cannot be empty ");
+            }
+
+            #endregion
+
+            #region Create New Player
+
+            var newPlayer = new TblPlayer
+            {
+                PlayerId = newPlayerId,
+                PlayerName = request.PlayerName,
+                Email = request.Email
+            };
+
+            await _context.TblPlayers.AddAsync(newPlayer);
+            await _context.SaveChangesAsync();
+
+            #endregion
+
+            return Result<PlayerRequestModel>.Success(request);
         }
-
-        public async Task<Result<PlayerRequestModel>> CreatePlayerAsync(PlayerRequestModel request)
+        catch (Exception ex)
         {
-            try
-            {
-                #region Find Last Player
-
-                var lastPlayer = await _context.TblPlayers
-                    .OrderByDescending(p => p.PlayerId)
-                    .FirstOrDefaultAsync();
-
-                #endregion
-
-                #region Create Player Id
-
-                int nextId = 1;
-                if (lastPlayer is not null && lastPlayer.PlayerId.StartsWith("p"))
-                {
-                    int.Parse(lastPlayer.PlayerId.Substring(1));
-                    nextId++;
-                }
-
-                string newPlayerId = $"p{nextId}";
-
-                #endregion
-
-                #region Validation for Player Name and Email
-
-                if (string.IsNullOrEmpty(request.PlayerName)) 
-                {
-                    return Result<PlayerRequestModel>.ValidationError("Player Name cannot be empty ");
-                }
-
-                if(string.IsNullOrEmpty(request.Email))
-                {
-                    return Result<PlayerRequestModel>.ValidationError("Email cannot be empty ");
-                }
-
-                #endregion
-
-                #region Create New Player
-
-                var newPlayer = new TblPlayer
-                {
-                    PlayerId = newPlayerId,
-                    PlayerName = request.PlayerName,
-                    Email = request.Email
-                };
-
-                await _context.TblPlayers.AddAsync(newPlayer);
-                await _context.SaveChangesAsync();
-
-                #endregion
-
-                return Result<PlayerRequestModel>.Success(request);
-            }
-            catch (Exception ex)
-            {
-                return Result<PlayerRequestModel>.SystemError("An error occurred while creating the player: " + ex.Message);
-            }
+            return Result<PlayerRequestModel>.SystemError("An error occurred while creating the player: " + ex.Message);
         }
     }
 }
